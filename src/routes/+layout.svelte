d9d349adffb9a6f4d0fd59665f89379caf5ff0b8
moko256 <koutaro.mo@gmail.com>
2026-08-21 13:59:22 +0900

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,6 @@
 
 <div style="display:none">
 	{#each locales as locale (locale)}
-		<a
-			href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}
-		>{locale}</a>
+		<a href={resolve(localizeHref(page.url.pathname, { locale }) as Pathname)}>{locale}</a>
 	{/each}
 </div>
