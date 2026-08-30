bcc14184a97682ed4b7bee63d2690f1a9afa0f67
moko256 <koutaro.mo@gmail.com>
2026-08-31 03:42:27 +0900

    chore: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+</script>
+
+<main class="container">
+	<h1>{page.status} {page.error?.message}</h1>
+	<a href={resolve('/')}>Back to TOP</a>
+</main>
